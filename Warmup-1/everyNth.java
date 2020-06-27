34d6d6b35f05eb2d2b53143d373dc2721c28927a
IcyWolF <63017582+TTVMrWang@users.noreply.github.com>
2020-06-26 17:29:46 -0700

    Create everyNth.java

---
+++ Warmup-1/everyNth.java
@@ -0,0 +1,12 @@
+public String everyNth(String str, int n) {
+  int nSize = (int) Math.ceil((double) str.length() / n);
+  char[] nchar = new char[nSize];
+  int index = 0;
+
+  for(int i = 0; i < str.length(); i += n) {
+      nchar[index] = str.charAt(i);
+      index++;
+  }
+ 
+  return new String(nchar);
+}
