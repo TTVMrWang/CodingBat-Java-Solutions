8031089314b23327f31cdc4262292d103a565d0a
IcyWolF <63017582+TTVMrWang@users.noreply.github.com>
2020-06-27 20:42:10 -0700

    Create inOrderEqual.java

---
+++ Logic-1/inOrderEqual.java
@@ -0,0 +1,14 @@
+public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
+  if(a < b && b < c && !equalOk){
+    return true;
+  }
+  
+  if(a <= b && b <= c && !equalOk){
+    return false;
+  }
+  if(a <= b && b <= c && equalOk){
+    return true;
+  }
+  
+  return false;
+}
