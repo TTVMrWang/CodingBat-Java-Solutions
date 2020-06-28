88d2214689682c847dcd5ec19bd7af24defa02b0
IcyWolF <63017582+TTVMrWang@users.noreply.github.com>
2020-06-27 21:41:57 -0700

    Create lastTwo.java

---
+++ String-1/lastTwo.java
@@ -0,0 +1,9 @@
+public String lastTwo(String str) {
+	int len = str.length();
+	if (len >= 2) {
+		return str.substring(0, len - 2) + str.charAt(len - 1) + str.charAt(len - 2);
+	}
+	else {
+	  return str;
+	}
+}
