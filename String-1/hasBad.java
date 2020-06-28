6c04f1b972db589319f8b37cfc156364d2b33408
IcyWolF <63017582+TTVMrWang@users.noreply.github.com>
2020-06-27 20:57:55 -0700

    Create hasBad.java

---
+++ String-1/hasBad.java
@@ -0,0 +1,13 @@
+public boolean hasBad(String str) {
+  if (str.length() <= 2) {
+    return false;
+  }
+  if (str.length() == 3 && !str.equals("bad")){
+    return false;
+  }
+  if ((str.substring(0, 3).equals("bad")) || (str.substring(1, 4).equals("bad"))){
+    return true;
+  }
+  
+  return false;
+}
