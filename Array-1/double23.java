7dad6359fc91c29cea66596e83e2bf34d6a7bff0
IcyWolF <63017582+TTVMrWang@users.noreply.github.com>
2020-07-03 16:37:26 -0700

    Create double23.java

---
+++ Array-1/double23.java
@@ -0,0 +1,9 @@
+public boolean double23(int[] nums) {
+  if(nums.length == 2) {
+		if(nums[0] == 2 && nums[1] == 2)
+			return true;
+		return (nums[0] == 3 && nums[1] == 3);
+	}
+	
+	return false;
+}
