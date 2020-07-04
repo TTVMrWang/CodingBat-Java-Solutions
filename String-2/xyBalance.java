91297cb11cbed864badc2caef41b033701ecbbad
IcyWolF <63017582+TTVMrWang@users.noreply.github.com>
2020-07-04 13:26:28 -0700

    Create xyBalance.java

---
+++ String-2/xyBalance.java
@@ -0,0 +1,14 @@
+public boolean xyBalance(String str) {
+  int len = str.length() - 1;
+	char ch;
+	for(int i = len; i >= 0; i--)
+	{
+		ch = str.charAt(i);
+		if(ch == 'x')
+			return false;
+		else if(ch == 'y')
+			return true;
+	}
+	
+	return true;  
+}
