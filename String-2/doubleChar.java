a01bd8662866f6f54ce4bac6aa696380cc795694
IcyWolF <63017582+TTVMrWang@users.noreply.github.com>
2020-07-17 16:01:41 -0700

    Update doubleChar.java

--- String-2/doubleChar.java
+++ String-2/doubleChar.java
@@ -1,13 +1,9 @@
 public String doubleChar(String str) {
-  char[] arr = new char[2 * str.length()];
-    int count = 0;
-      
-    for(int i = 0; i < str.length(); i++) {
-        arr[count] = str.charAt(i);
-        count++;
-        arr[count] = str.charAt(i);
-        count++;
-    }
-                            
-    return new String(arr);
+  String s = "";
+    
+  for (int i = 0; i < str.length(); i++) {
+    s += str.substring(i, i + 1) + str.substring(i, i + 1);
+  }
+  
+  return s;
 }
